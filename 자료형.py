339a5ce7615a7b1146383455193bd39263c66651
darkrigard <xarkrigard@naver.com>
2022-12-18 20:09:31 +0900

    String Practice

---
+++ 자료형.py
@@ -0,0 +1,65 @@
+a = 123
+
+b = 3
+
+a = 5
+
+c = 1.4
+
+print(a + c)
+
+print(a * b)
+
+# print(a ** b) 거듭제곱
+
+# print(a % b) 나머지 반환
+
+# print(7 // 4) 몫 반환
+
+print(14 % 3)
+
+
+food = "Python's favorite food is perl"
+
+print(food)
+
+say = '"Python is very easy" he says'
+
+print(say)
+
+say1 = "\" Python is very easy. \" he says"
+
+print(say1)
+
+multiline = "Life is too short\nYou need python"
+
+print(multiline)
+
+multiline1 = '''Life is too short
+You need python'''
+
+print(multiline1)
+
+say2 = "i will python\nkeep going"
+
+say3 = '''i want hope doing well
+in Python'''
+
+print(say2)
+print(say3)
+
+head = "Python"
+
+tail = "is fun!"
+
+print(head + tail)
+
+a = "python"
+
+print(a * 2)
+
+print("=" * 50)
+print("multistring test")
+print("=" * 50)
+
+print(len(a))
