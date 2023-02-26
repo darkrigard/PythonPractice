4ebcb0281ed35e3998f43c96eefb76eded93dfeb
darkrigard <xarkrigard@naver.com>
2023-02-26 17:50:59 +0900

    python_practice

--- Practice.py
+++ Practice.py
@@ -1,83 +1,54 @@
-
 class FourCal:
 
     def __init__(self, first, second):
         self.first = first
         self.second = second
 
-    def setdata(self, first, second):
-        self.first = first
-        self.second = second
-    
     def add(self):
         result = self.first + self.second
         return result
     
+    def sub(self):
+        result = self.first - self.second
+        return result
+    
+    def div(self):
+        if self.second == 0:
+            return 0
+        
+        else:
+            result = self.first / self.second
+            return result
     def mul(self):
         result = self.first * self.second
         return result
 
-    def sub(self):
-        result = self.first - self.second
-        return result
-
-    def div(self):
-        result = self.first / self.second
-        return result
-
 class MoreFourCal(FourCal):
+
     def pow(self):
         result = self.first ** self.second
         return result
 
+class SafeFourCal(MoreFourCal):
 
-# a = FourCal(5,10)
-
-# print(a.first)
-# # a.setdata(4,2)
-
-# print(a.add())
-
-# print(a.mul())
-
-# print(a.sub())
-
-# print(a.div())
-
-# a = MoreFourCal(4,2)
-
-# print(a.pow())
-
-# a = FourCal(4,0)
-
-# print(a.div())
-
-class SafeFourCal(FourCal):
-    def div(self):
-        if self.second == 0:
-            return 0
-
+    def mul(self):
+        if self.first > 100:
+            return "숫자가 너무 큽니다."
         else:
-            return self.first / self.second
+            result = self.first * self.second
+            return result
+        
 
-# b = SafeFourCal(4,0)
+a = SafeFourCal(101, 6)
 
-# print(b.div())
+print(a.add())
 
-class Family:
-    lastname = "김"
+print(a.div())
+
+print(a.mul())
+
+print(a.sub())
+
+print(a.pow())
 
 
-print(Family.lastname)
-
-a = Family()
-b = Family()
-
-print(a.lastname)
-print(b.lastname)
-
-a.lastname = "최"
-
-print(a.lastname)
-
-print(Family.lastname)
